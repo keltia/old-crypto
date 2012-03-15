7350f849893acd70687e4b04f4d699e36ec836c6
Ollivier Robert <roberto@keltia.net>
2012-03-15 22:23:31 +0100

    Add benchmarks for #condensed.

--- misc/bench.rb
+++ misc/bench.rb
@@ -2,7 +2,7 @@ require "benchmark"
 
 require "crypto_helper"
 
-N = 10_000  
+N = 100_000  
 Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|  
   b.report('to_numeric') do  
     N.times { "ANTICONSTITUTIONNELLEMENT".to_numeric }  
@@ -16,4 +16,37 @@ Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|
   b.report('to_numeric11') do  
     N.times { "ANTICONSTITUTIONNELLEMENT".to_numeric11 }  
   end  
-end  
+end
+
+class String
+  def condensed
+    c_alpha = ''
+    
+    self.scan(/./) do |c|
+      if not c_alpha.include?(c) then
+        c_alpha = c_alpha + c
+      end
+    end
+    c_alpha
+  end # -- condensed
+  
+  def condensed1
+    c_alpha = self.each_char.inject("") {|s,c|
+      if s.include?(c)
+        c=''
+      end
+      s+c
+    }
+  end # -- condensed
+end
+
+Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|
+  b.report("condensed") do
+    N.times { "ANTICONSTITUTIONNELLEMENT".condensed }
+  end
+  b.report("condensed1") do
+    N.times { "ANTICONSTITUTIONNELLEMENT".condensed1 }
+  end
+end
+
+  
