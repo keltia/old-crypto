aeb832b561744b3f419ad11d006133829a0ac0d5
Ollivier Robert <roberto@keltia.net>
2012-03-16 01:04:31 +0100

    More benchmarks.

--- misc/bench.rb
+++ misc/bench.rb
@@ -2,7 +2,7 @@ require "benchmark"
 
 require "crypto_helper"
 
-N = 100_000  
+N = 10_000  
 Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|  
   b.report('to_numeric') do  
     N.times { "ANTICONSTITUTIONNELLEMENT".to_numeric }  
@@ -18,35 +18,48 @@ Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|
   end  
 end
 
+M = 100_000
+Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|
+  b.report("condensed") do
+    M.times { "ANTICONSTITUTIONNELLEMENT".condensed }
+  end
+end
+
 class String
-  def condensed
-    c_alpha = ''
-    
-    self.scan(/./) do |c|
-      if not c_alpha.include?(c) then
-        c_alpha = c_alpha + c
+  def replace_double(letter = "Q")
+    a_str = self.split(//)
+    i = 0
+    while i < a_str.length do
+      if a_str[i] == a_str[i+1] then
+        a_str[i+1] = letter
       end
+      i += 1
     end
-    c_alpha
-  end # -- condensed
-  
-  def condensed1
-    c_alpha = self.each_char.inject("") {|s,c|
-      if s.include?(c)
-        c=''
+    a_str.join.to_s
+  end # -- replace_double
+
+  def replace_double1(letter = "Q")
+    self.each_char.inject("") {|s,c|
+      if s[-1] == c
+        c = letter
       end
       s+c
     }
-  end # -- condensed
+  end # -- replace_double
+
 end
 
+M = 100_000
 Benchmark.benchmark(' ' * 31 + Benchmark::Tms::CAPTION, 31) do |b|
   b.report("condensed") do
-    N.times { "ANTICONSTITUTIONNELLEMENT".condensed }
+    M.times { "ANTICONSTITUTIONNELLEMENT".replace_double }
   end
   b.report("condensed1") do
-    N.times { "ANTICONSTITUTIONNELLEMENT".condensed1 }
+    M.times { "ANTICONSTITUTIONNELLEMENT".replace_double1 }
   end
 end
+
+puts "ANTICONSTITUTIONNELLEMENT".replace_double
+puts "ANTICONSTITUTIONNELLEMENT".replace_double1
 
   
