8dc84f997c9d3348d7d1343ade658f9190700c2b
Ollivier Robert <roberto@keltia.net>
2013-03-10 19:24:53 +0100

    Remove dead tests.  Move require properly.

--- lib/key.rb
+++ lib/key.rb
@@ -6,7 +6,10 @@
 # Author:: Ollivier Robert <roberto@keltia.freenix.fr>
 # Copyright:: © 2001-2009 by Ollivier Robert 
 #
-# $Id: key.rb,v 4a72faeea033 2013/03/10 18:23:32 roberto $
+# $Id: key.rb,v 2c285ab71d4e 2013/03/10 18:24:53 roberto $
+
+class DataError < Exception
+end
 
 require 'key/base'
 require 'key/chaokey'
@@ -18,37 +21,3 @@ require 'key/sckey'
 require 'key/sqkey'
 require 'key/wheatstone'
 require 'key/tkey'
-
-class DataError < Exception
-end
-
-if $0 == __FILE__ then
-  
-  # several test keys
-  #
-  # square
-  #
-  k = Key::SCKey.new('ARABESQUE')
-  p k.condensed
-  
-  # not square but known -- see above comments
-  #
-  m = Key::SCKey.new('subway')
-  p m.condensed
-  
-  # not square
-  #
-  n = Key::SCKey.new('portable')
-  p n.condensed
-
-  # key for transposition
-  #
-  t = Key::TKey.new('retribution')
-  #
-  #
-  # Main usage, get the numerical order of letters
-  #
-  p t.to_numeric
-
-  exit(0)
-end
